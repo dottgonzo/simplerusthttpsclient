0aac049d6b38201c188b7b743d5934936f79e7b2
dottg0n20 <dev@dariocaruso.info>
2023-10-30 01:15:36 +0100

    ref

--- src/tests/nodeserver/cert.sh
+++ src/tests/nodeserver/cert.sh
@@ -1,6 +1,6 @@
 ################### this works for localhost ###################
 
-openssl req -x509 -newkey rsa:4096 -days 3650 -keyout ca_key.pem -out ca_cert.pem -subj "/CN=MyCA" -nodes
+openssl req -x509 -newkey rsa:4096 -days 36500 -keyout ca_key.pem -out ca_cert.pem -subj "/CN=MyCA" -nodes
 openssl genpkey -algorithm RSA -out server_key.pem
 openssl req -new -key server_key.pem -out server_csr.pem -subj "/CN=localhost"
-openssl x509 -req -in server_csr.pem -CA ca_cert.pem -CAkey ca_key.pem -CAcreateserial -out server_cert.pem -days 3650 -extfile <(echo -e "subjectAltName=DNS:localhost\nkeyUsage=digitalSignature,keyEncipherment\nextendedKeyUsage=serverAuth")
+openssl x509 -req -in server_csr.pem -CA ca_cert.pem -CAkey ca_key.pem -CAcreateserial -out server_cert.pem -days 36500 -extfile <(echo -e "subjectAltName=DNS:localhost\nkeyUsage=digitalSignature,keyEncipherment\nextendedKeyUsage=serverAuth")
